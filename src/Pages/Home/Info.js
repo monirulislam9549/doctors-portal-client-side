1086126ead05e2a0eb3dfbde854752c5a8e915e3
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-10 15:42:46 +0600

    info card modified

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -6,10 +6,10 @@ import call from '../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard cardDescription="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
-            <InfoCard cardDescription="Brooklyn, NY 10036, United States" cardTitle="Our location" img={marker}></InfoCard>
-            <InfoCard cardDescription="+000 123 456789" cardTitle="Contact Us" img={call}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardDescription="Lorem Ipsum is simply dummy text of the pri" cardTitle="Opening Hours" img={clock}></InfoCard>
+            <InfoCard bgClass="bg-accent" cardDescription="Brooklyn, NY 10036, United States" cardTitle="Our location" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardDescription="+000 123 456789" cardTitle="Contact Us" img={call}></InfoCard>
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InfoCard = ({ img, cardTitle, cardDescription }) => {
+const InfoCard = ({ img, cardTitle, cardDescription, bgClass }) => {
     return (
-        <div class="card card-side bg-base-100 shadow-xl bg-accent">
+        <div class={`card card-side bg-base-100 shadow-xl ${bgClass}`}>
             <figure className='pl-5'><img src={img} alt="Movie" /></figure>
             <div class="card-body text-white">
                 <h2 class="card-title">{cardTitle}</h2>
-                <p>{cardDescription}</p>
+                <p className='description'>{cardDescription}</p>
             </div>
         </div>
     );
