70a6783dc73c69daf127aec9889c16333b9bc7e2
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-19 11:15:06 +0600

    react hook form install and added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,18 +1,55 @@
 import React from 'react';
 import auth from '../../firebase.init';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useForm } from "react-hook-form";
+
 
 const Login = () => {
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
     if (user) {
         console.log(user);
     }
+
+    const onSubmit = data => {
+        console.log(data)
+    };
+
+
     return (
         <div className='flex justify-center items-center h-screen'>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <div className="card-body">
                     <h2 className="text-center font-bold text-2xl">Login</h2>
+                    <form onSubmit={handleSubmit(onSubmit)}>
 
+                        <div class="form-control w-full max-w-xs">
+                            <label class="label">
+                                <span class="label-text">Email</span>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Your Email"
+                                class="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    pattern: {
+                                        value: /[A-Za-z]{3}/,
+                                        message: 'error message'
+                                    }
+                                })}
+                            />
+                            <label class="label">
+                                <span class="label-text-alt">Alt label</span>
+                            </label>
+                        </div>
+
+                        <input />
+                        {errors.firstName?.type === 'required' && "First name is required"}
+                        <input {...register("lastName", { required: true })} />
+                        {errors.lastName && "Last name is required"}
+                        <input type="submit" />
+                    </form>
                     <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
