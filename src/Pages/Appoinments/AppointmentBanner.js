008e0abffced4522c5a8a050cb2ac4fff6bbd5db
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-19 10:12:50 +0600

    google authentication completed

--- src/Pages/Appoinments/AppointmentBanner.js
+++ src/Pages/Appoinments/AppointmentBanner.js
@@ -5,9 +5,9 @@ import 'react-day-picker/dist/style.css';
 
 const AppointmentBanner = ({ date, setDate }) => {
     return (
-        <div style={{ backgroundImage: `url(${bg})` }} class="hero min-h-screen">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
+        <div style={{ backgroundImage: `url(${bg})` }} className="hero min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='Dentist Chair' />
                 <div> <DayPicker
                     mode="single"
                     selected={date}
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import auth from '../../firebase.init';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user);
+    }
     return (
-        <div>
-            <h1>This is Login</h1>
+        <div className='flex justify-center items-center h-screen'>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="text-center font-bold text-2xl">Login</h2>
+
+                    <div className="divider">OR</div>
+                    <button
+                        onClick={() => signInWithGoogle()}
+                        className="btn btn-outline">Continue With Google</button>
+
+                </div>
+            </div>
         </div>
     );
 };
