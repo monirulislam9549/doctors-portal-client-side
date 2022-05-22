904073e1193698c0ada67712c5df273c6637b80a
monirulislam9549 <monirulislam9549@gmail.com>
2022-05-22 12:27:09 +0600

    require admin created

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import MyAppointment from './Pages/Dashboard/MyAppointment';
 import MyReview from './Pages/Dashboard/MyReview';
 import MyHistory from './Pages/Dashboard/MyHistory';
 import Users from './Pages/Dashboard/Users';
+import RequiredAdmin from './Pages/Login/RequiredAdmin';
 
 function App() {
   return (
@@ -32,7 +33,7 @@ function App() {
           <Route index element={<MyAppointment></MyAppointment>}></Route>
           <Route path='review' element={<MyReview> </MyReview>}></Route>
           <Route path='history' element={<MyHistory></MyHistory>}></Route>
-          <Route path='users' element={<Users></Users>}></Route>
+          <Route path='users' element={<RequiredAdmin><Users></Users></RequiredAdmin>}></Route>
         </Route>
 
         <Route path='/signup' element={<SignUp></SignUp>}></Route>
--- src/Pages/Dashboard/UserRow.js
+++ src/Pages/Dashboard/UserRow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { toast } from 'react-toastify';
 
-const UserRow = ({ user, refetch }) => {
+const UserRow = ({ user, refetch, index }) => {
     const { email, role } = user;
     const makeAdmin = () => {
         fetch(`http://localhost:5000/user/admin/${email}`, {
@@ -10,15 +10,22 @@ const UserRow = ({ user, refetch }) => {
                 authorization: `Bearer ${localStorage.getItem('accessToken')}`
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                if (res.status === 403) {
+                    toast.error('Failed to Make an Admin');
+                }
+                return res.json()
+            })
             .then(data => {
-                refetch();
-                toast.success(`Successfully made an Admin`)
+                if (data.modifiedCount > 0) {
+                    refetch();
+                    toast.success(`Successfully made an Admin`)
+                }
             })
     }
     return (
         <tr>
-            <th>1</th>
+            <th>{index + 1}</th>
             <td>{email}</td>
             <td>{role !== 'admin' && <button onClick={makeAdmin} class="btn btn-xs">Make Admin</button>}</td>
             <td><button class="btn btn-xs">Remove User</button></td>
